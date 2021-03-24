876c83ff66cb35a613d0ef8d439d4a871454d141
Elina <orstho11@gmail.com>
2021-03-24 21:50:40 +0100

    Added Product,category, position etc in models.py: Added static directory, HTML file updated with stypes,menu, searchbaretc

--- store/bboard/models.py
+++ store/bboard/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your models here.
+class Customer(models.Model):
+    #ID_user_profile = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    surname = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=17)
+
+    class Meta:
+        db_table = "customer"
+
+    #def __str__(self):
+     #   return self.surname + " " + self.name
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    # ID default: ID_user_profile = models.AutoField(primary_key=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    amount = models.IntegerField()
+    price = models.FloatField()
+
+    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Purchase(models.Model):
+    id_customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+class Purchase_Position(models.Model):
+    amount = models.IntegerField()
+    id_product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    id_purchase = models.ForeignKey(Purchase,on_delete=models.CASCADE)
--- store/bboard/views.py
+++ store/bboard/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def bboard(requiest):
-    return render(requiest,'basis/index.html')
+    return render(requiest,'bboard/basis/index.html')
+
